ff0f126ea981296fd36a908a9a166a1a82eb0b40
s16501 <yuki.a.0924.lovya@keio.jp>
2017-04-27 12:39:36 +0900

    表か裏かマンに勝つコードを追加
    うまく作動しませんでした・・・

--- docs/player.js
+++ docs/player.js
@@ -1,3 +1,30 @@
 function action(oppornent){
-  return 1;
+  return 0;
 }
+const GU = 0;
+const CHOKI = 1;
+const PA = 2;
+ function action(){
+   return GU;
+ }
+
+ let matches = 0;
+ 
+ function action(opporment){
+   matches = matches + 1;
+   let amari = matches % 2;
+   console.log(amari);
+   return GU;
+ }
+
+ let matches = 0;
+ function action(opporment){
+   matches = matches + 1;
+   const remainder = matches % 2;
+   const isEven = remainder == 0;
+   console.log(`${matches}) % 2 = ${remainder}:${isEven}`);
+   if(isEven){
+     return GU 
+   }
+   return CHOKI; 
+ }
