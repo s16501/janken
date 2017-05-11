83362f1960956ff3f085d7490799e584231a213d
s16501 <yuki.a.0924.lovya@keio.jp>
2017-05-11 11:28:56 +0900

    abs(-10)の変化を確認

--- docs/player.js
+++ docs/player.js
@@ -1,3 +1,4 @@
+/*
 function action(oppornent){
   return 0;
 }
@@ -16,15 +17,29 @@ const PA = 2;
    console.log(amari);
    return GU;
  }
+*/
+
+const GU = 0;
+const CHOKI = 1;
+const PA = 2;
 
  let matches = 0;
  function action(opporment){
    matches = matches + 1;
-   const remainder = matches % 2;
-   const isEven = remainder == 0;
-   console.log(`${matches}) % 2 = ${remainder}:${isEven}`);
+   const remainder = matches % 3;
+   const isEven = remainder == PA;
+   console.log(`${matches}) % 3 = ${remainder}:${isEven}`);
    if(isEven){
-     return GU 
+     return CHOKI; 
    }
-   return CHOKI; 
+   return GU; 
  }
+
+
+function abs(value){
+  let result = value;
+  if(result < 0){
+    result = result * -1;
+  }
+  return result;
+}
